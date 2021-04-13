f0992dce202008edebd519cd4e4585d65ede8bd8
Philip Meier <pmeier@quansight.com>
2021-04-13 09:57:15 +0200

    factor out tests utilities

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,63 +1,16 @@
-import pytest
+from .utils import Config, make_params
 
 
-class Config:
-    def __init__(
-        self,
-        id,
-        *,
-        new_cmds,
-        legacy_cmds=None,
-        passed=0,
-        skipped=0,
-        failed=0,
-        errors=0,
-        xpassed=0,
-        xfailed=0,
-    ):
-        self._id = id
-        self._new_cmds = self._parse_cmds(new_cmds)
-        self._legacy_cmds = self._parse_cmds(legacy_cmds or self._new_cmds)
-        self._outcomes = dict(
-            passed=passed,
-            skipped=skipped,
-            failed=failed,
-            errors=errors,
-            xpassed=xpassed,
-            xfailed=xfailed,
-        )
-
-    @staticmethod
-    def _parse_cmds(cmds):
-        return (cmds,) if isinstance(cmds, str) else cmds
-
-    @staticmethod
-    def _amend_cmds(cmds, file):
-        if not cmds or not cmds[0].startswith(":"):
-            return (file, *cmds)
-        else:
-            return (file + cmds[0], *cmds[1:])
-
-    def make_params(self, file):
-        return (
-            pytest.param(
-                file,
-                self._amend_cmds(self._new_cmds, file),
-                self._outcomes,
-                id=f"{self._id}-new",
-            ),
-            pytest.param(
-                file,
-                self._amend_cmds(self._legacy_cmds, file),
-                self._outcomes,
-                id=f"{self._id}-legacy",
-            ),
-        )
-
-
-FILE = "test_device.py"
-CONFIGS = [
-    Config("*testcase-*test-*device", new_cmds=(), passed=3, skipped=2, failed=3),
+@make_params(
+    "test_device.py",
+    Config(
+        "*testcase-*test-*device",
+        new_cmds=(),
+        legacy_cmds=(),
+        passed=3,
+        skipped=2,
+        failed=3,
+    ),
     Config(
         "1testcase1-*test-*device",
         new_cmds="::TestFoo",
@@ -130,17 +83,7 @@ CONFIGS = [
         legacy_cmds="::TestFooCPU::test_bar_cpu",
         passed=1,
     ),
-]
-
-
-def make_params(file, configs):
-    return pytest.mark.parametrize(
-        ("file", "cmds", "outcomes"),
-        [param for config in configs for param in config.make_params(file)],
-    )
-
-
-@make_params(FILE, CONFIGS)
+)
 def test_collection(testdir, file, cmds, outcomes):
     testdir.copy_example(file)
     result = testdir.runpytest(*cmds)
