fa9624cd3973bf4c2f4e79c8b101812bc9241bbf
Philip Meier <pmeier@quansight.com>
2021-04-20 10:16:23 +0200

    make dtype testing more concise (#15)
    * make dtype testing more concise
    
    Instead of instantiating the tests for all devices and use the `@onlyCPU` decorator everywhere, used the `only_for` keyword when instantiating. With that the meta tests are not generated in the first place and do not need to be considered for the number of skipped tests.
    
    * cleanup

--- tests/assets/test_dtype.py
+++ tests/assets/test_dtype.py
@@ -4,20 +4,17 @@ import torch
 from torch.testing._internal.common_device_type import (
     dtypes,
     instantiate_device_type_tests,
-    onlyCPU,
 )
 from torch.testing._internal.common_utils import TestCase
 
 
 class TestFoo(TestCase):
-    @onlyCPU
     @dtypes(torch.float16, torch.int32)
     # fails for float16, passes for int32
     def test_bar(self, device, dtype):
         assert dtype == torch.int32
 
     # passes for float16, skips for int32
-    @onlyCPU
     @dtypes(torch.float16, torch.int32)
     def test_baz(self, device, dtype):
         if dtype == torch.int32:
@@ -26,18 +23,16 @@ class TestFoo(TestCase):
         assert True
 
 
-instantiate_device_type_tests(TestFoo, globals())
+instantiate_device_type_tests(TestFoo, globals(), only_for="cpu")
 
 
 class TestSpam(TestCase):
-    @onlyCPU
     def test_ham(self, device):
         assert True
 
-    @onlyCPU
     @dtypes(torch.float16)
     def test_eggs(self, device, dtype):
         assert False
 
 
-instantiate_device_type_tests(TestSpam, globals())
+instantiate_device_type_tests(TestSpam, globals(), only_for="cpu")
