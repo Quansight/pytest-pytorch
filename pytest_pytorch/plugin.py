30e22904678ec6f5143013150d7b9b04572d31ce
Philip Meier <pmeier@quansight.com>
2021-04-21 12:03:10 +0200

    support selection of tests that use op infos

--- pytest_pytorch/plugin.py
+++ pytest_pytorch/plugin.py
@@ -1,13 +1,12 @@
+import inspect
 import re
 import unittest.mock
 import warnings
-from typing import Pattern
 
 from _pytest.unittest import TestCaseFunction, UnitTestCase
 
 try:
-    from torch.testing._internal.common_device_type import get_device_type_test_bases
-    from torch.testing._internal.common_utils import TestCase as PyTorchTestCaseTemplate
+    from torch.testing._internal.common_utils import TestCase as TestCaseTemplate
 
     TORCH_AVAILABLE = True
 except ImportError:
@@ -17,93 +16,73 @@ except ImportError:
         "Disabling the plugin 'pytest-pytorch', because 'torch' could not be imported."
     )
 
-    def get_device_type_test_bases():
-        return []
 
-    class PyTorchTestCaseTemplate:
-        pass
-
-
-class PytestPyTorchInternalError(Exception):
-    def __init__(self, msg):
-        super().__init__(
-            f"{msg}\n"
-            f"This is an internal error of the pytest plugin 'pytest-pytorch'."
-            f"If you encounter this during normal operation, please file an issue "
-            f"https://github.com/Quansight/pytest-pytorch/issues."
-        )
-
-
-TEST_BASE_DEVICE_PATTERN = re.compile(r"(?P<device>\w*?)TestBase$")
-
-
-def _get_devices():
-    devices = []
-    for test_base in get_device_type_test_bases():
-        match = TEST_BASE_DEVICE_PATTERN.match(test_base.__name__)
-        if not match:
-            raise PytestPyTorchInternalError(
-                f"Unable to extract device name from {test_base.__name__}"
-            )
-
-        devices.append(match.group("device"))
-
-    return devices
-
-
-DEVICES = _get_devices()
-
-
-class TemplateName(str):
-    _TEMPLATE_NAME_PATTERN: Pattern
-
-    def __init__(self, _):
-        super().__init__()
-        match = self._TEMPLATE_NAME_PATTERN.match(self)
-        if not match:
-            raise PytestPyTorchInternalError(
-                f"Unable to extract template name from {self}"
-            )
-        self._template_name = match.group("template_name")
+class TemplatedName(str):
+    def __new__(cls, name, template_name):
+        self = super().__new__(cls, name)
+        self._template_name = template_name
+        return self
 
     def __eq__(self, other):
-        return str.__eq__(self, other) or str.__eq__(self._template_name, other)
+        exact_match = str.__eq__(self, other)
+        if exact_match:
+            return True
 
-    def __hash__(self) -> int:
+        if not self._template_name:
+            return False
+
+        return str.__eq__(self._template_name, other)
+
+    def __hash__(self):
         return super().__hash__()
 
 
-class TestCaseFunctionTemplateName(TemplateName):
-    _TEMPLATE_NAME_PATTERN = re.compile(
-        fr"(?P<template_name>\w*?)_({'|'.join([device.lower() for device in DEVICES])})"
-    )
+class TemplatedTestCaseFunction(TestCaseFunction):
+    _TEMPLATE_NAME_PATTERN = re.compile(r"def (?P<template_name>test_\w+)\(")
 
-
-class PyTorchTestCaseFunction(TestCaseFunction):
     @classmethod
-    def from_parent(cls, parent, *, name, **kw):
+    def _extract_template_name(cls, callobj):
+        if not callobj:
+            return None
+
+        match = cls._TEMPLATE_NAME_PATTERN.search(inspect.getsource(callobj))
+        if not match:
+            return None
+
+        return match.group("template_name")
+
+    @classmethod
+    def from_parent(cls, parent, *, name, callobj, **kw):
         return super().from_parent(
-            parent, name=TestCaseFunctionTemplateName(name), **kw
+            parent, name=TemplatedName(name, cls._extract_template_name(callobj)), **kw
         )
 
 
-class TestCaseTemplateName(TemplateName):
-    _TEMPLATE_NAME_PATTERN = re.compile(
-        fr"(?P<template_name>\w*?)({'|'.join([device.upper() for device in DEVICES])})"
-    )
+class TemplatedTestCase(UnitTestCase):
+    @classmethod
+    def _extract_template_name(cls, name, obj):
+        if not obj:
+            return None
 
+        if not hasattr(obj, "device_type"):
+            return None
 
-class PyTorchTestCase(UnitTestCase):
+        return name[: -len(obj.device_type)]
+
     @classmethod
     def from_parent(cls, parent, *, name, obj=None):
-        return super().from_parent(parent, name=TestCaseTemplateName(name), obj=obj)
+        return super().from_parent(
+            parent,
+            name=TemplatedName(name, cls._extract_template_name(name, obj)),
+            obj=obj,
+        )
 
     def collect(self):
         # Yes, this is a bad practice. Unfortunately, there is no other option to
         # inject our custom 'TestCaseFunction' without duplicating everything in
         # 'UnitTestCase.collect()'
         with unittest.mock.patch(
-            "_pytest.unittest.TestCaseFunction", new=PyTorchTestCaseFunction
+            "_pytest.unittest.TestCaseFunction", new=TemplatedTestCaseFunction
         ):
             yield from super().collect()
 
@@ -113,12 +92,9 @@ def pytest_pycollect_makeitem(collector, name, obj):
         return None
 
     try:
-        if (
-            not issubclass(obj, PyTorchTestCaseTemplate)
-            or obj is PyTorchTestCaseTemplate
-        ):
+        if not issubclass(obj, TestCaseTemplate) or obj is TestCaseTemplate:
             return None
     except Exception:
         return None
 
-    return PyTorchTestCase.from_parent(collector, name=name, obj=obj)
+    return TemplatedTestCase.from_parent(collector, name=name, obj=obj)
